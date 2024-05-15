242d6c762011d5ad3527955cad793edad223d511
dvirak <dvirak43@vt.edu>
2024-05-14 20:04:04 -0400

    Initial Seed Data

--- DB/seedData.js
+++ DB/seedData.js
@@ -14,6 +14,36 @@ async function dropTables() {
 async function createTables() {
   try {
     console.log("BUILDING TABLES...");
+
+    await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      first_name VARCHAR(255) NOT NULL,
+      last_name VARCHAR(255) NOT NULL,
+      preferred_name VARCHAR(255) NULL,
+      phone BIGINT NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      admin BOOLEAN NOT NULL DEFAULT false
+    );
+      `);
+
+    await client.query(`
+    CREATE TABLE products(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) UNIQUE NOT NULL,
+      author VARCHAR(255) NOT NULL,
+      artist VARCHAR(255) NOT NULL,
+      publisher VARCHAR(255) NOT NULL,
+      publish_date DATE NOT NULL,
+      description VARCHAR NOT NULL,
+      genre VARCHAR(255) NOT NULL,
+      series_name VARCHAR(255) NULL,
+      series_volume SMALLINT NULL,
+      cover_image VARCHAR(255) NOT NULL,
+    );
+    `);
   } catch (error) {
     throw error;
   }
