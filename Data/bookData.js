171f6a177bb4a6b3129f93af722793ee335300e4
dvirak <dvirak43@vt.edu>
2024-05-16 16:56:07 -0400

    Initial User & Book Seed Data

---
+++ Data/bookData.js
@@ -0,0 +1,28 @@
+const books = [
+  {
+    id: 1,
+    title: "Watchmen",
+    author: "Alan Moore",
+    illustrator: "Dave Gibbons",
+    penciler: "",
+    inker: "",
+    colorist: "John Higgins",
+    letterer: "Dave Gibbons",
+    publisher: "DC Comics",
+    publish_date: "1987-01-01",
+    description:
+      "Set in an alternate reality on the brink of nuclear war, this graphic novel follows a group of retired masked vigilantes as they uncover a sinister conspiracy threatening humanity. Through a gritty narrative and morally ambiguous characters, it explores themes of power, morality, and existentialism.",
+    print_length: 416,
+    genre1: "Science Fiction",
+    genre2: "Fantasy",
+    genre3: "Dystopia",
+    series_name: "Watchmen",
+    series_volume: "1-12",
+    cover_image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1442239711i/472331.jpg",
+  },
+];
+
+module.exports = {
+  books,
+};
