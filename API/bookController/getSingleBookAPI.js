68151f1957ea3d3a30dd318eeed920888a842b79
dvirak <dvirak43@vt.edu>
2024-06-03 19:12:43 -0400

    Cleaned getSingleBookAPI and bookController/index

--- API/bookController/getSingleBookAPI.js
+++ API/bookController/getSingleBookAPI.js
@@ -15,22 +15,20 @@ const {
  * @param {Object} res - The response object, used to send back the desired book data or an error message.
  */
 async function getSingleBookAPI(req, res) {
-  const { book_id, book_name } = req.body;
-
-  console.log(req.body);
+  const { book_id, book_title } = req.body;
 
   console.log("IN GET SINGLE BOOK API");
 
   try {
     // Validate input
-    if (!book_id && !book_name) {
+    if (!book_id && !book_title) {
       return res
         .status(400)
-        .json({ error: "Either book_id or book_name must be provided." });
+        .json({ error: "Either book_id or book_title must be provided." });
     }
 
     // Call the database function to get the book data
-    const book = await getSingleBookDB(book_id, book_name);
+    const book = await getSingleBookDB(book_id, book_title);
 
     // Send the book data as the response
     res.status(200).json(book);
