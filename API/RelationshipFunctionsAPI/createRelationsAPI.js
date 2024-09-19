d3f5b69ccaa0d8d7ad6c0f349243891d983b3269
dvirak <dvirak43@vt.edu>
2024-09-18 20:11:35 -0400

    createRelationsAPI initial feature

---
+++ API/RelationshipFunctionsAPI/createRelationsAPI.js
@@ -0,0 +1,28 @@
+const createAdditionalInfoDB = require("../../DB/DBFunctions/BookDB/helpers/createAdditionalInfoDB");
+const { logErrorAPI, MissingInformationErrorAPI } = require("../../Errors/API");
+const { formatAdditionalInfo } = require("../bookController/Helpers");
+
+async function createRelationsAPI(req, res, next) {
+  console.log("IN createRelationsAPI");
+  let relations = req.body;
+  let book_id = req.params.id;
+
+  try {
+    if (!relations || !book_id) {
+      throw new MissingInformationErrorAPI(
+        "You are missing information needed for this query. Please double check the information provided"
+      );
+    }
+
+    let updatedRelation = await createAdditionalInfoDB(book_id, relations);
+
+    let formattedRelation = formatAdditionalInfo(updatedRelation);
+
+    res.status(200).json(formattedRelation);
+  } catch (error) {
+    logErrorAPI("createRelationsAPI", error, next);
+    throw error;
+  }
+}
+
+module.exports = createRelationsAPI;
